ec61782a01709203a36374e44abc3feaee7c8e27
minjay <mjfan@ucar.edu>
2016-01-06 04:21:47 -0800

    rearrange

--- Scripts/user_xgb.py
+++ Scripts/user_xgb.py
@@ -13,7 +13,7 @@ session = pd.read_csv(my_dir+'sessions.csv')
 labels = df_train['country_destination'].values
 id_test = df_test['id']
 
-df_train = df_train.drop(['country_destination'], axis=1)
+df_train.drop(['country_destination'], axis=1, inplace=True)
 
 n_train = df_train.shape[0]
 # ignore the old index
@@ -27,7 +27,7 @@ df_all.loc[df_all['age']>1000, 'age'] = 2015-df_all.loc[df_all['age']>1000, 'age
 df_all.loc[(df_all['age']>100) | (df_all['age']<18), 'age'] = -1
 df_all['age'].fillna(-1, inplace=True)
 
-# preprocess data_account_created
+# preprocess date_account_created
 # Question: treat dates as continuous or categorical?
 dac = np.vstack(df_all['date_account_created'].apply(lambda x: list(map(int, x.split('-')))).values)
 df_all['dac_year'] = dac[:, 0]
@@ -40,6 +40,7 @@ tfa = df_all['timestamp_first_active']
 df_all['tfa_year'] = tfa.apply(lambda x: int(str(x)[0:4]))
 df_all['tfa_month'] = tfa.apply(lambda x: int(str(x)[4:6]))
 df_all['tfa_day'] = tfa.apply(lambda x: int(str(x)[6:8]))
+# keep hr, min, sec in the model
 df_all['tfa_hr'] = tfa.apply(lambda x: int(str(x)[8:10]))
 df_all['tfa_min'] = tfa.apply(lambda x: int(str(x)[10:12]))
 df_all['tfa_sec'] = tfa.apply(lambda x: int(str(x)[12:14]))
@@ -51,30 +52,34 @@ features = ['gender', 'signup_method', 'signup_flow', 'language', 'affiliate_cha
 'first_affiliate_tracked', 'signup_app', 'first_device_type', 'first_browser']
 for f in features:
   df_all_dum = pd.get_dummies(df_all[f], prefix=f)
-  df_all = df_all.drop(f, axis=1)
+  df_all.drop(f, axis=1, inplace=True)
   df_all = pd.concat([df_all, df_all_dum], axis=1)
 
 # session
 session.rename(columns = {'user_id': 'id'}, inplace=True)
+# replace NaN by -1
 session['action'].fillna(-1, inplace=True)
 session['action_type'].fillna(-1, inplace=True)
 session['action_detail'].fillna(-1, inplace=True)
 session['device_type'].fillna(-1, inplace=True)
+##
 session['secs_elapsed'].fillna(0, inplace=True)
 action = pd.pivot_table(session, values='secs_elapsed', index='id', columns='action', aggfunc=len,
 	fill_value=0)
 action.rename(columns=lambda x: 'action_'+str(x), inplace=True)
+# action_type does not help
+# there are only a few different action_type
+# difficult to distinguish
 action_detail = pd.pivot_table(session, values='secs_elapsed', index='id', columns='action_detail', aggfunc=len,
 	fill_value=0)
 action_detail.rename(columns=lambda x: 'action_detail_'+str(x), inplace=True)
 
+##
 action_sec = pd.pivot_table(session, values='secs_elapsed', index='id', columns='action', aggfunc=np.sum,
   fill_value=0)
 action_sec.rename(columns=lambda x: 'action_sec_'+str(x), inplace=True)
 
-action = pd.pivot_table(session, values='secs_elapsed', index='id', columns='action', aggfunc=len,
-	fill_value=0)
-
+# number of different actions for each user
 grouped = session[['id', 'action']].groupby('id')
 myfun = lambda x: len(pd.Series(x).value_counts())
 action_num = grouped.aggregate(myfun)
@@ -83,6 +88,7 @@ action_num = grouped.aggregate(myfun)
 df_all = df_all.join(action, on='id')
 df_all = df_all.join(action_detail, on='id')
 df_all = df_all.join(action_num, on='id')
+##
 df_all = df_all.join(action_sec, on='id')
 df_all.fillna(-1, inplace=True)
 
@@ -116,7 +122,7 @@ def ndcg5(preds, dtrain):
   ndcg = np.mean((np.apply_along_axis(cal_dcg, 1, rel)))
   return 'ndcg5', -ndcg
 
-## setup parameters for xgb
+## specify parameters for xgb
 param = {}
 # use softmax multi-class classification
 param['objective'] = 'multi:softprob'
@@ -145,7 +151,6 @@ for i in range(len(id_test)):
 	idx = id_test[i]
 	ids += [idx] * 5
 
-# Generate submission
 sub = pd.DataFrame(data={'id':ids, 'country':cts}, columns=['id', 'country'])
 my_dir = os.getcwd()+'/Airbnb/Subs/'
 sub.to_csv(my_dir+'sub.csv', index=False)
