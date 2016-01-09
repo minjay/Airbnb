587291f478c3eefa846a8c1e68d53d527657963d
minjay <mjfan@ucar.edu>
2016-01-08 22:54:26 -0800

    random forest code

---
+++ Scripts/user_rf.py
@@ -0,0 +1,11 @@
+# Random Forest
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(n_estimators=1000, n_jobs=20, random_state=0, verbose=1)
+rf.fit(X_train, y_train)
+
+preds = rf.predict_proba(X_val)
+ndcg5(preds, xg_val)
+
+y_pred = rf.predict_proba(X_test)
